166d61e3cbb38dd32f071431668b3cccedaceb65
riya751885 <rg751885@gmail.com>
2024-03-29 22:38:00 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-violet-500 py-4">
+        <h1 className="text-4xl font-bold text-center text-Black font-bold">Top Courses</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
